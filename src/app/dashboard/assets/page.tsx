9e1174fcee5a2e7156bcf7c21e0c757e7116b1b5
Mohammed Bilal N S <bilalnsmuhammed@gmail.com>
2025-09-11 13:26:41 +0530

    feat: upload assets

--- src/app/dashboard/assets/page.tsx
+++ src/app/dashboard/assets/page.tsx
@@ -1,4 +1,25 @@
+import { getCategoriesAction, getUserAssetsAction } from "@/actions/dashboard-actions";
+import AssetsGrid from "@/components/dashboard/asset-grid";
+import UploadAssets from "@/components/dashboard/upload-assets";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-export default function Assets(){
-	return <div>Assets</div>
+export default  async function UserAssetsPage(){
+	const session = await auth.api.getSession({
+		headers: await headers()
+	})
+	if(!session) return null 
+	
+	const [categories, assets] = await Promise.all([getCategoriesAction(), getUserAssetsAction(session?.user.id)])
+	console.log(assets)
+	return (
+
+		<div className="container py-6">
+			<div className="flex justify-between items-center mb-6">
+				<h1 className="text-3xl font-extrabold">My Assets</h1>
+				<UploadAssets categories={categories || []} />
+			</div>
+			<AssetsGrid/>
+		</div>
+	)
 }
---
+++ src/components/dashboard/asset-grid.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export default function  AssetsGrid(){
+	return <div>Asset Grid</div>
+}
